18fe5cde3d010867616eec59ccdbdf6dacd1368e
egrizzell <elisegrizzell@gmail.com>
2017-10-02 12:09:01 -0500

    changed order of css docs

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
 
 
     <!-- Bootstrap core CSS -->
-    <link href="css\bootstrap.css" rel="stylesheet">
+    <link href="css/agency.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -20,7 +21,6 @@
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
     <!-- Custom styles for this template -->
-    <link href="css/agency.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
     
   </head>
